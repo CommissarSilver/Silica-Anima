ce25c98ead6a0593fdeb8a20e20e57cb28bf7ee3
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-12-15 16:17:50 -0500

    praise be

--- Schola Machinalis Discendi/Linear-Logistic Regression/utils.py
+++ Schola Machinalis Discendi/Linear-Logistic Regression/utils.py
@@ -1,42 +1,49 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
-def create_dataset(
-    number_of_samples: int = 1000,
-    number_of_features: int = 2,
-    noise: float = 0.1,
-):
+def binary_dataset(num_samples: int = 100):
     """
-    Create a dataset with two classes for binary classification.
-
-    Args:
-        number_of_samples (int, optional): number of samples to create. Defaults to 1000.
-        number_of_features (int, optional): number of features for each datapoint. Defaults to 2.
-        noise (float, optional): noise to add to the linearity of the funciton. Defaults to 0.1.
+    Generates a binary dataset of size num_samples.
     """
-    X = np.random.uniform(-1, 1, size=(number_of_samples, number_of_features))
-    # add some noise to the linearity of the function
-    # X[:, 1] = X[:, 1] + np.random.normal(0, noise, size=(number_of_samples,))
-    # create a linearity
-    Y = np.zeros(number_of_samples)
-    if np.sum(X[:, 1] > 0) > np.sum(X[:, 1] < 0):
-        Y[X[:, 1] > 0] = 1
-    else:
-        Y[X[:, 1] < 0] = 1
-
-    return X, Y
+    # Generate random points in the unit square
+    points = np.random.rand(num_samples, 2)
+    # Assign a label to each point
+    labels = np.array([1 if (x[0] > x[1]) else -1 for x in points])
+    return points, labels
 
 
-def sigmoid(x: float):
-    """
-    Sigmoid function.
+X, Y = binary_dataset(200)
 
-    Args:
-        x (float): function input
+weights = np.zeros(2)
+bias = np.zeros(1)
 
-    Returns:
-        float: application of the sigmoid function to x
-    """
-    return 1 / (1 + math.exp(-x))
+learning_rate = 0.001
+losses = []
+epochs = 10000
 
+
+def sigmoid(z: np.array):
+    return 1 / (1 + np.exp(-z))
+
+
+j = sigmoid(0)
+
+for epoch in range(epochs):
+    y_hat = sigmoid(np.dot(X, weights) + bias)
+    error = y_hat - Y
+    
+    delta_w_1 = np.dot(X[:, 0], error) / len(X)
+    delta_w_2 = np.dot(X[:, 1], error) / len(X)
+    delta_b = np.sum(error) / len(X)
+
+    weights[0] -= learning_rate * delta_w_1
+    weights[1] -= learning_rate * delta_w_2
+    bias -= learning_rate * delta_b
+
+    loss = np.sum(-1 * ((Y * np.log(y_hat)) + ((1 - Y) * np.log(1 - y_hat)))) / len(X)
+    losses.append(loss)
+
+plt.plot(losses)
+plt.show()
